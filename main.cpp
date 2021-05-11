7671a9bcc40cb4c7173dbd60167f1df378d0935d
MorawskiR <r.morawski92@gmail.com>
2021-05-11 22:49:41 +0200

    added string

--- main.cpp
+++ main.cpp
@@ -4,29 +4,34 @@
 #include "header.hpp"
 int main()
 {
-    std::string haslo = "PPSDASD#33";
-
-    if(checkifupper(haslo))
+    std::string haslo1;
+    std::string haslo2;
+    std::cout<<"podaj haslo1: ";
+    std::cin>>haslo1;
+    std::cout<<"podaj haslo1: ";
+    std::cin>>haslo2;
+    
+    if(checkifupper(haslo1))
     {
         std::cout<<"posiada duza ";
     }
     std::cout<<"\n";
-    if(checkspecialchar(haslo))
+    if(checkspecialchar(haslo1))
     {
         std::cout<<"posiada special ";
     }
         std::cout<<"\n";
 
-     if(checkNumber(haslo))
+
+    if(checkNumber(haslo1))
     {
         std::cout<<"posiada liczbe ";
     }
         std::cout<<"\n";
 
-    if (checklenght(haslo))
+
+    if (checklenght(haslo1))
     {
         std::cout<<"dlugosc ok ";
-    }
-    
-    
+    }   
 }
